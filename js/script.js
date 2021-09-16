8471f36b65f0ca0392d45863379554136e3c2a1a
Tina2010 <t.rittmueller@gmx.de>
2021-09-16 19:39:12 +0200

    [feat] adding feature to add new item to to do list

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 function newItem() {
 
-    let list = $('#list');
-    let li = $('<li></li>');
-    let inputValue = $('input').val();
+let list = $('#list');
+let li = $('<li></li>');
+let inputValue = $('input').val();
 
+li.append(inputValue);
+
+    //1. Adding a new item to the list of items:
+
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        list.append(li);
+    };
 
 };
